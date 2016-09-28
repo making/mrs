d5a31e194354a4587dbd2615eb751680ed14782d
making <makingx@gmail.com>
2016-09-28 11:19:19 +0900

    Fix

--- cherry-pick-all.sh
+++ cherry-pick-all.sh
@@ -2,7 +2,7 @@
 
 commitid=$1
 
-branches=`git branch -l | tr '*' ' '`
+branches="01-cf-push 02-scale-out 03-auto-recover 04-log-aggregation 05-session-cluster 06-metrics 07-zero-downtime-push"
 for b in $branches
 do
     if [ "$b" != "00-beginning" ];then
@@ -12,3 +12,4 @@ do
         git push origin $b
     fi
 done
+git checkout 00-beginning
