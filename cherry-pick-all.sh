24ffe2020330b9267c9e959e3cf74ed5669ebfff
making <makingx@gmail.com>
2016-09-28 11:11:55 +0900

    Add script

---
+++ cherry-pick-all.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+commitid=
+
+branches=`git branch -l | tr '*' ' '`
+
+for b in $branches
+do
+    if [ "$b" != "00-beginning" ];then
+        echo "==== $b ===="
+        git checkout $b
+        git cherry-pick -x $commitid
+        git push origin $b
+    fi
+done
