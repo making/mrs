7bf68507482b789a6aa79a071a2679f822024660
making <makingx@gmail.com>
2016-09-28 11:21:34 +0900

    Polish

--- cherry-pick-all.sh
+++ cherry-pick-all.sh
@@ -5,11 +5,9 @@ commitid=$1
 branches="01-cf-push 02-scale-out 03-auto-recover 04-log-aggregation 05-session-cluster 06-metrics 07-zero-downtime-push"
 for b in $branches
 do
-    if [ "$b" != "00-beginning" ];then
-        echo "==== $b ===="
-        git checkout $b
-        git cherry-pick -x $commitid
-        git push origin $b
-    fi
+    echo "==== $b ===="
+    git checkout $b
+    git cherry-pick -x $commitid
+    git push origin $b
 done
 git checkout 00-beginning
