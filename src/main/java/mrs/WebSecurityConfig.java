7e6c3c77d3b989c03ea6d3a7ce138ad08ab31c11
Toshiaki Maki <tmaki@pivotal.io>
2019-07-01 13:37:14 +0900

    Fix typo

--- src/main/java/mrs/WebSecurityConfig.java
+++ src/main/java/mrs/WebSecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecutiryConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/js/**", "/css/**").permitAll()
