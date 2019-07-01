62f307919ebe1572222913d8a956a1521e088e8f
Toshiaki Maki <tmaki@pivotal.io>
2019-07-01 13:37:46 +0900

    Use Configuration instead of EnableWebSecurity

--- src/main/java/mrs/WebSecurityConfig.java
+++ src/main/java/mrs/WebSecurityConfig.java
@@ -1,11 +1,11 @@
 package mrs;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity
+@Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
